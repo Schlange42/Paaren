b33dcade3b7e15421d5be2699ba5104c5a5f1fbe
Tim <tim.arbeit@yahoo.de>
2013-02-15 21:03:00 +0100

    Paarungen

--- Tourney.java
+++ Tourney.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.math.*;
 
 class Tourney {
 
@@ -34,4 +35,25 @@ class Tourney {
         // this updates all maps with new mms, sos, sosos
     }
 
+
+
+    public void makepairings() {
+        updateScores();
+        double[][] edges = new double[players.size()][(players.size()*(players.size()-1))/2];
+        double[] cost = new double[(players.size()*(players.size()-1))/2];
+        double[] b = new double[players.size()];
+        double[] solution = new double[(players.size()*(players.size()-1))/2];
+        int k=0;
+        for(int i=0; i<players.size(); i++) {
+            b[i]=1;
+            for(int j=i+1; j<players.size(); j++) {
+                edges[i][k]=1;
+                edges[j][k]=1;
+                k++;
+                cost[k]=1/(Math.abs(mms.get(players.get(i))-mms.get(players.get(j))));
+            }
+        }
+        Simplex sp = new Simplex(edges,b,cost); 
+        solution = sp.primal();
+    }
 }
